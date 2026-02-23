10117cf000bc73d3bbc3755d445b8ed62a774163
SLI0124 <sli0124@vsb.cz>
2026-02-23 20:06:52 +0100

    refactor(config): remove visibility from weather variables list since it's empty

--- pipeline/utils/config.py
+++ pipeline/utils/config.py
@@ -56,7 +56,6 @@ WEATHER_VARIABLES = [
     "surface_pressure",
     "dew_point_2m",
     "cloud_cover",
-    "visibility",
     "wind_speed_10m",
     "wind_direction_10m",
     "apparent_temperature",
